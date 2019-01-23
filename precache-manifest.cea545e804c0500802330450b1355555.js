ebba4fd9d4ce6a69525d2b3a1dbb477c7bf6c38d
Shilu <shilendrasoman@gmail.com>
2019-01-23 16:24:29 +0530

    Updates

---
+++ precache-manifest.cea545e804c0500802330450b1355555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "654c6eb1e86dcd47f882",
+    "url": "/tic-tac-toe/static/js/main.654c6eb1.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "654c6eb1e86dcd47f882",
+    "url": "/tic-tac-toe/static/css/main.de68b44b.chunk.css"
+  },
+  {
+    "revision": "83b92d3a9ed6367796ff1e0bcb51acdc",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
